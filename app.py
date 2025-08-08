ec56aa699e10a69482eeb742b66c352859cfe3c3
Fanvarli <69014400+Fanvarli@users.noreply.github.com>
2025-08-08 15:54:46 +0400

    app.py

--- app.py
+++ app.py
@@ -4,19 +4,18 @@ import os
 app = Flask(__name__)
 
 CONFIRMATION_TOKEN = os.environ.get("CONFIRMATION_TOKEN")
-GROUP_TOKEN = os.environ.get("GROUP_TOKEN")
-BOT_NAME = os.environ.get("BOT_NAME", "dizel")
 
 @app.route("/", methods=["POST"])
 def vk_callback():
     data = request.get_json(force=True)
     if data.get("type") == "confirmation":
-        # Возвращаем токен с кодом 200
-        return make_response(CONFIRMATION_TOKEN, 200)
+        # возвращаем строку подтверждения с кодом 200 и content-type text/plain
+        response = make_response(CONFIRMATION_TOKEN, 200)
+        response.headers["Content-Type"] = "text/plain"
+        return response
     elif data.get("type") == "message_new":
-        # Обработка сообщений
-        return make_response("ok", 200)
-    return make_response("ok", 200)
+        return "ok", 200
+    return "ok", 200
 
 @app.route("/", methods=["GET"])
 def check():
